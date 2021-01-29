69ee52c4b5c71d0b185f60f7e7e9908464516586
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-01-29 10:49:35 +0200

    showing flash alerts

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -3,6 +3,7 @@
 <div class="row">
     <div class="col-md-6 mx-auto">
         <div class="card card-body bg-light mt-5">
+            <?php flash('register_fail'); ?>
             <h2>Create an account</h2>
             <p>Please fill in the form to register with us</p>
             <form action="" method="post">
