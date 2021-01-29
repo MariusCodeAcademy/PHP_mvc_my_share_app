78302f58ae14517ee569fdbc23e7a3af6e592298
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-01-29 08:12:36 +0200

    create userModel in controller

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -7,8 +7,11 @@
 */
 class Users extends Controller
 {
+    private $userModel;
+
     public function __construct()
     {
+        $this->userModel = $this->model('User');
     }
 
     public function register()
