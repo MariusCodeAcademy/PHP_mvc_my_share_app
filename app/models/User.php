c1757b84f420bbd250f35da738f2358e652bba4f
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-01-29 11:55:27 +0200

    login fn to check if we have email ant pass in db.

--- app/models/User.php
+++ app/models/User.php
@@ -52,4 +52,30 @@ class User
             return false;
         }
     }
+
+    // Checks in the database for the email and password
+    // tries to verify password
+    // return row or false 
+    public function login($email, $notHashedPass)
+    {
+        // get the row whith given email 
+        $this->db->query("SELECT * FROM users WHERE `email` = :email");
+
+        $this->db->bind(':email', $email);
+
+        $row = $this->db->singleRow();
+
+        if ($row) {
+            $hashedPassword = $row->password;
+        } else {
+            return false;
+        }
+
+        // check password
+        if (password_verify($notHashedPass, $hashedPassword)) {
+            return $row;
+        } else {
+            return false;
+        }
+    }
 }
