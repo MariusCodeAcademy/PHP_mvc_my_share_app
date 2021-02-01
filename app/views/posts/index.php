927c27101fa99ffc251d3d46840429274fb3b116
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-01 11:01:42 +0200

    generate post cards in the view.

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -5,12 +5,31 @@
         <h1>Posts</h1>
     </div>
     <div class="col">
-        <a href="<?php echo URLROOT; ?>/posts/add" class="btn btn-primary pull-right">
+        <a href="<?php echo URLROOT; ?>/posts/add" class="btn btn-primary pull-right mt-2">
             <i class="fa fa-pencil"></i>
             Add post
         </a>
     </div>
 </div>
+
+<div class="row">
+    <?php foreach ($data['posts'] as $post) :  ?>
+        <div class="col-md-6">
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h4 class="card-title"><?php echo $post->title ?></h4>
+                    <p class="bg-light p-2 mb-3"> Written By <?php echo $post->name ?></p>
+                    <p class="card-text"><?php echo $post->body ?></p>
+                    <a href="<?php echo URLROOT . '/posts/show/' . $post->postId; ?>" class="card-link">Read more</a>
+                </div>
+                <div class="card-footer">Created at: <?php echo $post->postCreated ?></div>
+            </div>
+        </div>
+    <?php endforeach; ?>
+
+</div>
+
+
 
 
 
