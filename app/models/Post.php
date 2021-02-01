eaf8ae1cf25ab029c311d534b240b9513c5a8dba
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-01 11:00:00 +0200

    joined sql query to get relevant data

--- app/models/Post.php
+++ app/models/Post.php
@@ -14,7 +14,18 @@ class Post
     // @return Object Array
     public function getPosts()
     {
-        $this->db->query("SELECT * FROM posts");
+        $sql = "SELECT posts.title, posts.body, users.name, users.email,
+        posts.id as postId,
+        users.id as userId,
+        posts.created_at as postCreated,
+        users.created_at as userCreated
+        FROM posts
+        INNER JOIN users
+        ON posts.user_id = users.id
+        ORDER BY posts.created_at DESC";
+
+
+        $this->db->query($sql);
 
         $result = $this->db->resultSet();
 
