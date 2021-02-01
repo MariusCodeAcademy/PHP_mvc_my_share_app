d0d55425a892ba19bc08e83074604b2cda4cdd1b
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-01 11:00:57 +0200

    create postModel, use getPosts and send data with the view

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -8,15 +8,23 @@
 
 class Posts extends Controller
 {
+    private $postModel;
+
     public function __construct()
     {
         // restrict access of this controller only to logged in users
         if (!isLoggedIn()) redirect('/users/login');
+
+        $this->postModel = $this->model('Post');
     }
 
     public function index()
     {
-        $data = [];
+        // get posts 
+        $posts = $this->postModel->getPosts();
+        $data = [
+            'posts' => $posts
+        ];
         $this->view('posts/index', $data);
     }
 }
