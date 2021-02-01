c0c02d36151cfcb3c1a2eb1317bc55995f367286
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-01 08:20:32 +0200

    session helper class fix

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -21,6 +21,7 @@ function flash($name = '', $message = '', $class = 'alert alert-success')
 
             // this is where we display msg to the view
         } elseif (empty($message) && !empty($_SESSION[$name])) {
+            $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
             echo "<div class='$class' id='msg-flash'>{$_SESSION[$name]}</div>";
             // unset the values that have been shown
             unset($_SESSION[$name]);
