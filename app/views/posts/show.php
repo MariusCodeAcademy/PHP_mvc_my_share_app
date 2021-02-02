82ff7e7b5ec8ea128e07bfb288ceb339de806f00
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-02 09:01:43 +0200

    singe post view in progress

---
+++ app/views/posts/show.php
@@ -0,0 +1,9 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<h1>This is single page template for the post <?php echo $data['post_id'] ?></h1>
+
+
+
+
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
