2fc18d2276abf765cf3fdb288dab8465b2c7fcb0
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-02 10:07:48 +0200

    show single post in progress

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -1,7 +1,13 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
+<a href="<?php echo URLROOT ?>/posts" class='btn btn-light my-3'> <i class="fa fa-chevron-left"></i> Back</a>
+<br />
 
-<h1>This is single page template for the post <?php echo $data['post_id'] ?></h1>
-
+<h1 class="display-3"><?php echo $data['post']->title ?></h1>
+<div class="bg-secondary text-white p-2 mb-3">
+    Written by <strong><?php echo $data['user']->name ?></strong>
+    On: <?php echo $data['post']->created_at ?>
+</div>
+<p class="lead"><?php echo $data['post']->body ?></p>
 
 
 
