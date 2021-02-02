203f4f47a05c9c8441e56d52c1039a9645aa8f11
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-02 10:42:30 +0200

    show edit and delete only if post belongs to current user

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -9,6 +9,16 @@
 </div>
 <p class="lead"><?php echo $data['post']->body ?></p>
 
+<!-- show this only if this post belongs to this user -->
+<?php if ($_SESSION['user_id'] === $data['post']->user_id) : ?>
+    <hr>
+    <a href="<?php echo URLROOT . '/posts/edit/' . $data['post']->id ?>" class='btn btn-info '> <i class="fa fa-pencil"></i> Edit</a>
+
+    <form action="<?php echo URLROOT ?>/posts/delete" method="post" class='pull-right'>
+        <button type="submit" class="btn btn-danger"><i class="fa fa-close"></i> Delete</button>
+    </form>
+<?php endif; ?>
+
 
 
 
