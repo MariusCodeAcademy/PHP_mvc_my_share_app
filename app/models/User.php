6c7c85ccce9a7f3b42a7ece6d18b02a747252351
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-02 10:07:25 +0200

    created getUserById() mthd

--- app/models/User.php
+++ app/models/User.php
@@ -78,4 +78,18 @@ class User
             return false;
         }
     }
+
+    // will return user data row if found
+    // return false if not found
+    public function getUserById($id)
+    {
+        $this->db->query("SELECT name, email FROM users WHERE id = :id");
+
+        $this->db->bind(':id', $id);
+
+        $row = $this->db->singleRow();
+
+        if ($this->db->rowCount() > 0) return $row;
+        return false;
+    }
 }
