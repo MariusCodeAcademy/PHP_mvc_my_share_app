2bce2feb302b3c65d463fa7538105613c34ab0af
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-03 09:34:53 +0200

    sanitize and validion methods

--- app/libraries/Validation.php
+++ app/libraries/Validation.php
@@ -11,4 +11,19 @@ class Validation
         if ($_SERVER['REQUEST_METHOD'] === "POST") return true;
         return false;
     }
+
+    public function sanitizePost()
+    {
+        // sanitize Post Array
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+    }
+
+    public function ifRequestIsPostAndSanitize()
+    {
+        if ($this->ifRequestIsPost()) {
+            $this->sanitizePost();
+            return true;
+        }
+        return false;
+    }
 }
