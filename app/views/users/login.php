cbf5bbd45d9ddac34971472d7530be4ada2c7681
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-04 10:33:00 +0200

    fix erros structure

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -8,13 +8,13 @@
             <form action="" method="post">
                 <div class="form-group">
                     <label for="email">Email:<sup>*</sup></label>
-                    <input type="email" name="email" id="email" class="<?php echo (!empty($data['emailErr'])) ? 'is-invalid' : ''; ?> form-control form-control-lg" value="<?php echo $data['email'] ?>">
-                    <span class='invalid-feedback'><?php echo $data['emailErr'] ?></span>
+                    <input type="email" name="email" id="email" class="<?php echo (!empty($data['errors']['emailErr'])) ? 'is-invalid' : ''; ?> form-control form-control-lg" value="<?php echo $data['email'] ?>">
+                    <span class='invalid-feedback'><?php echo $data['errors']['emailErr'] ?></span>
                 </div>
                 <div class="form-group">
                     <label for="password">Password:<sup>*</sup></label>
-                    <input type="password" name="password" id="password" class="<?php echo (!empty($data['passwordErr'])) ? 'is-invalid' : ''; ?> form-control form-control-lg" value="<?php echo $data['password'] ?>">
-                    <span class='invalid-feedback'><?php echo $data['passwordErr'] ?></span>
+                    <input type="password" name="password" id="password" class="<?php echo (!empty($data['errors']['passwordErr'])) ? 'is-invalid' : ''; ?> form-control form-control-lg" value="<?php echo $data['password'] ?>">
+                    <span class='invalid-feedback'><?php echo $data['errors']['passwordErr'] ?></span>
                 </div>
 
                 <div class="row">
