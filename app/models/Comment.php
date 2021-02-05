14d6a7e7880e0bb7bc33e5a6b2ddd8f410dd1508
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-05 10:26:29 +0200

    get comments by post id in model

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -7,5 +7,18 @@ class Comment
     {
         $this->db = new Database;
     }
-    
+
+    public function getMeComments($post_id)
+    {
+        $this->db->query('SELECT * FROM comments WHERE post_id = :id');
+
+        $this->db->bind(':id', $post_id);
+
+        $comments = $this->db->resultSet();
+
+        if ($this->db->rowCount() > 0) {
+            return $comments;
+        }
+        return false;
+    }
 }
