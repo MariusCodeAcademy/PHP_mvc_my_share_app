de6cd26b04304dec7a4de029a7f09fa4e2af9c18
mariusKuzminskasAdroiti <marius.kuzminskas@adroiti.com>
2021-02-08 09:27:32 +0200

    api add commet method data validation and flow.

--- app/controllers/API.php
+++ app/controllers/API.php
@@ -33,17 +33,68 @@ class API extends Controller
 
     public function addComment($post_id = null)
     {
-        $result = [];
-        print_r($_POST);
-        echo 'addComment ' . $post_id;
+        $result = [
+            'errors' => [],
+        ];
+
+        if ($post_id === null) {
+            $result['errors'] = 'no id given';
+            redirect('/posts');
+            // die();
+        }
+        // print_r($_POST);
+        // echo 'addComment ' . $post_id;
 
         // validate POST values
+        $vld = new Validation;
+        if ($vld->ifRequestIsPostAndSanitize()) {
+            // request is post and sanitized
+
+            $data = [
+                'username' => trim($_POST['username']),
+                'commentBody' => trim($_POST['commentBody']),
+                'errors' => [
+                    'usernameErr' => '',
+                    'commentBodyErr' => '',
+                ]
+            ];
+
+            // validate username
+            $data['errors']['usernameErr'] = $vld->validateEmpty($data['username'], 'Username can\'t be empty');
+            
+            $data['errors']['commentBodyErr'] = $vld->validateEmpty($data['commentBody'], 'Please enter your Comment');
+
+            if ($vld->ifEmptyArr($data['errors'])) {
+                // no errors
+                // execute add post from model and get result
+                $commentData = [
+                    'author' => $data['username'],
+                    'commentBody' => $data['commentBody'],
+                    'postId' => $post_id
+                ];
+                
+                if ($this->commentModel->addComment($commentData)) {
+                    $result['success'] = "Comment added";
+
+                } else {
+                    $result['errors'] = 'error adding post';
+                }
+
+            } else {
+                // create result 
+                $result['errors'] = $data['errors'];
+            }
+
+        } else {
+            // if request is not post
+            $result['error'] = "not alowed";
+            redirect('/posts');
+        }
 
         // $commentData = $_POST;
         // // if valid add post
-        // if ($this->commentModel->addComment($commentData)) return 'success';
-        $result['success'] = "Post added";
-
+        
+        header('Content-Type: application/json');
         echo json_encode($result);
         die();
     }
